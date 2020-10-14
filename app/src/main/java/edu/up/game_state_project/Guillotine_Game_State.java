48cc026d122ca968a6205f9dbe55749d4476ac74
William <cloutier23@up.edu>
2020-10-14 16:25:49 -0700

    Added more flesh to methods. Not fully completed

--- app/src/main/java/edu/up/game_state_project/Guillotine_Game_State.java
+++ app/src/main/java/edu/up/game_state_project/Guillotine_Game_State.java
@@ -4,20 +4,20 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Guillotine_Game_State {
-    private ArrayList<Card> p1Hand;
-    private ArrayList<Card> p1Field;
-    private ArrayList<Card> p0Hand;
-    private ArrayList<Card> p0Field;
-    private ArrayList<Card> nobleLine;
-    private int dayNum;
-    private int playerTurn;
-    private int p0Score;
-    private int p1Score;
-    private int lastFirstPlayer;
-    private int currFirstPlayer;
-    private ArrayList<Card> deckDiscard;
-    private ArrayList<Card> deckAction;
-    private ArrayList<Card> deckNoble;
-    private int turnPhase;
-    private String[] playerNames;
+    public ArrayList<Card> p1Hand;
+    public ArrayList<Card> p1Field;
+    public ArrayList<Card> p0Hand;
+    public ArrayList<Card> p0Field;
+    public ArrayList<Card> nobleLine;
+    public int dayNum;
+    public int playerTurn;
+    public int p0Score;
+    public int p1Score;
+    public int lastFirstPlayer;
+    public int currFirstPlayer;
+    public ArrayList<Card> deckDiscard;
+    public ArrayList<Card> deckAction;
+    public ArrayList<Card> deckNoble;
+    public int turnPhase;
+    public String[] playerNames;
 }
